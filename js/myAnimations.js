2f86ff0ae61cd5023f44dd57290b6fafccd00422
James Y. Rauhut <jamesrauhut@us.ibm.com>
2015-10-31 20:48:52 -0500

    Play States, Reverse, and Playback Rate

--- js/myAnimations.js
+++ js/myAnimations.js
@@ -1,14 +1,47 @@
-var shaq = document.getElementById('shaq');
+var animateShaq = document.getElementById('shaq').animate([
+    { transform: 'rotate(0)' },
+  { transform: 'rotate(360deg)' }
+  ], {
+    duration: 5000,
+    iterations: Infinity
+  }
+);
 
-var timings = {
-  duration: 5000,
-  iterations: Infinity
+var animateBatmobile = document.getElementById('batmobile').animate([
+    { transform: 'translateX(150%)' },
+    { transform: 'translateX(-150%)' }
+  ], {
+    duration: 4000,
+    iterations: 1
+  }
+);
+
+document.getElementById('play').onclick = function () {
+  animateBatmobile.play();
 };
 
-var keyframes = [
-  { transform: 'rotate(0)' },
-  { transform: 'rotate(360deg)' }
-];
+document.getElementById('pause').onclick = function () {
+  animateBatmobile.pause();
+};
 
-var shaqEffect = new KeyframeEffect(shaq, keyframes, timings);
-document.timeline.play(shaqEffect);
+document.getElementById('cancel').onclick = function () {
+  animateBatmobile.cancel();
+};
+
+document.getElementById('finish').onclick = function () {
+  animateBatmobile.finish();
+};
+
+document.getElementById('reverse').onclick = function () {
+  animateBatmobile.reverse();
+};
+
+document.getElementById('playbackRate').onclick = function () {
+  if (animateBatmobile.playbackRate === 1) {
+    animateBatmobile.playbackRate = 2;
+    document.getElementById('playbackRate').innerHTML = '.playbackRate = 1';
+  } else {
+    animateBatmobile.playbackRate = 1;
+    document.getElementById('playbackRate').innerHTML = '.playbackRate = 2';
+  }
+};
